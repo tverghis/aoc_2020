e8524bd549e6ff058cf131dd61344e94c37a876d
Tarun Verghis <tarun.verghis@gmail.com>
2020-12-05 21:21:45 -0500

    Use `.windows` for Part 2

--- aoc05/src/main.rs
+++ aoc05/src/main.rs
@@ -1,8 +1,7 @@
-use std::{collections::HashSet, cmp};
-
 const INPUT: &'static str = include_str!("../input.txt");
 fn main() {
-    let input_lines = INPUT.lines().collect::<Vec<_>>();
+    let mut input_lines = INPUT.lines().map(|b| find_seat_id(b)).collect::<Vec<_>>();
+    input_lines.sort_unstable();
 
     let p1 = part_1(&input_lines);
     println!("Part 1: {}", p1);
@@ -11,28 +10,12 @@ fn main() {
     println!("Part 1: {}", p2);
 }
 
-fn part_1(boarding_passes: &[&str]) -> usize {
-    boarding_passes.iter().map(|b| find_seat_id(b)).max().unwrap()
+fn part_1(seat_ids: &[usize]) -> usize {
+    *seat_ids.iter().last().unwrap()
 }
 
-fn part_2(boarding_passes: &[&str]) -> usize {
-    let mut min_id = usize::MAX;
-    let mut max_id = usize::MIN;
-
-    let mut found_seat_ids = HashSet::new();
-
-    for &b in boarding_passes {
-        let id = find_seat_id(b);
-        min_id = cmp::min(min_id, id);
-        max_id = cmp::max(max_id, id);
-
-        found_seat_ids.insert(id);
-    }
-
-    let valid_seat_ids = (min_id..=max_id).collect::<HashSet<_>>();
-
-    // There should be exactly one missing ID!
-    *valid_seat_ids.difference(&found_seat_ids).nth(0).unwrap()
+fn part_2(seat_ids: &[usize]) -> usize {
+    seat_ids.windows(2).find(|&w| w[1] != w[0] + 1).unwrap()[0] + 1
 }
 
 // The "sequences" (both row and column) are just binary numbers.
